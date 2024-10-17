f0328d75e63f9141f8b8f652be94a3403a8817e1
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-17 12:59:41 +0800

    numeric-2.py

---
+++ numeric-2.py
@@ -0,0 +1,12 @@
+#functions on numeric data
+pangkat = 3
+desimal_min = -69
+print(pow(desimal_min, pangkat))
+desimal = abs(desimal_min)
+print(desimal)
+
+print(bin(desimal))
+
+print(oct(desimal))
+
+print(hex(desimal))
